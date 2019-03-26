ad069f535cf9db786302b1760579cbecf8e211e0
pellegrinodavide <pellegrinodavide99@gmail.com>
2019-03-26 21:41:56 +0100

    Upgrade

--- app/src/main/java/com/davide/verbatiam/GamePanel.java
+++ app/src/main/java/com/davide/verbatiam/GamePanel.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
